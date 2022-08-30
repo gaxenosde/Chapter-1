846b8c72497fad808920d70c0383db2de7c79078
gaxenosde <110794666+gaxenosde@users.noreply.github.com>
2022-08-29 19:03:01 -0500

    Add files via upload

---
+++ Interfaz.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/*
+Programa que emula la interfaz de un cajero automático. El programa presentará las siguientes opciones al usuario de forma indefinida hasta que éste introduzca un número diferente a los indicados:
+
+Elija una de las siguientes opciones:
+    Escriba 1 para consultar su saldo
+    Escriba 2 para ingresar dinero
+    Escriba 3 para sacar dinero
+    Escriba 4 para consultar sus últimos movimientos
+Para salir escriba cualquier otro número
+Una vez escrita la opción, pulse la tecla Enter
+
+Una vez el usuario ha elegido la opción, el programa le mostrará por pantalla una frase explicando la opción elegida
+*/
+
+public class Interfaz {
+    public static void main(String[] args) {
+        System.out.println("Elija una de las siguientes opciones:");
+        System.out.println(" Escriba 1 para Consultar su saldo");
+        System.out.println(" Escriba 2 para Ingresar dinero");
+        System.out.println(" Escriba 3 para Sacar dinero");
+        System.out.println(" Escriba 4 para Consultar sus ultimos movimientos");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int opcionSeleccionada = scanner.nextInt();
+
+        System.out.println("La opcion que usted ha elegido es: " + opcionSeleccionada);
+
+    }
+}
